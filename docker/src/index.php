8197f3388cd1d2865246ec0c57919e2cc9c73f3e
lokesung <lokesung@gmail.com>
2019-01-05 00:06:57 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -2,9 +2,10 @@
 
 <html lang="en">
 <body>
-  <h1>My PHP Website</h1>
+  <h1>HCL Automation Devops</h1>
   <p>Here is some static content.</p>
-  <p><img src="https://www.w3schools.com/images/w3schools_green.jpg" 
+  <p><img 
+src="https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/0014/4052/brand.gif?itok=tIUANQkU" 
 alt="W3Schools.com"></p>
 </body>
 </html>
