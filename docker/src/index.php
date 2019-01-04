2cef2954e77b159287a056a7549e83e8f7f4abdf
lokesung <lokesung@gmail.com>
2019-01-04 21:20:13 +0800

    test

--- docker/src/index.php
+++ docker/src/index.php
@@ -1,9 +1,9 @@
 <html><head><title><?php echo "HCL Devops Automation";?></title></head>
 <body
 <?php 
-$image_url='https://www.naseba.com/wp-content/uploads/2018/09/HCL-Technologies.png';
+$image_url='/root/git/docker/src/HCL.png';
 ?>
 
 <img src="<?php echo $image_url;?>">
 
-<br><h1>Got me?</h1>
+<br><h1>HCL Devops</h1>
