24751435fd213ab407d3aca6d7e101ab467f7854
lokesung <lokesung@gmail.com>
2019-01-04 21:26:00 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -1,9 +1,6 @@
 <html><head><title><?php echo "HCL Devops Automation";?></title></head>
 <body
-<?php 
-$image_url='/root/git/docker/src/HCL.png';
-?>
 
-<img src="<?php echo $image_url;?>">
+<img src="/root/git/docker/src/HCL.png">
 
 <br><h1>HCL Devops</h1>
