4e97b56e7a54e0b5b758ad5982a477b49ea8d65a
lokesung <lokesung@gmail.com>
2019-01-04 22:49:18 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -1,17 +1,10 @@
 <!doctype html>
 
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>My PHP Website</title>
-</head>
-
 <body>
   <h1>My PHP Website</h1>
   <p>Here is some static content.</p>
-  <p><?php echo "Here is some dynamic content"; ?></p>
-  <p>
-  <?php
+  <p><?php echo "Here is some dynamic content";
   $img = '/root/git/docker/src/HCL.png';
 	header('Content-Type: image/jpeg');
 	readfile($img);
