fa36b1edee877991386eb5b1e6047a6c52252b4d
lokesung <lokesung@gmail.com>
2019-01-05 00:35:49 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -3,9 +3,9 @@
 <html lang="en">
 <body>
   <h1>HCL Automation Devops</h1>
-   <!-- Size: 140 px -- >
-<a href="/cliparts/T/5/d/2/w/R/hcl-logo-v3.svg.thumb.png"><img 
-src="/cliparts/T/5/d/2/w/R/hcl-logo-v3.svg.thumb.png" alt='Hcl Logo V3 clip art'/></a>
+   <p><img
+src="http://www.clker.com/cliparts/T/5/d/2/w/R/hcl-logo-v3.svg.med.png"
+alt="logo"></p>
   <p>This test site is auto build from Docker.</p>
   <p><img 
 src="https://cdn-images-1.medium.com/max/1200/1*CSZxfOMlVsKsrMkqTxFiMQ.png" 
