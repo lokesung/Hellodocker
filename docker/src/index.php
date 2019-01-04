71816eb17f79baafc624c2a7047a5276cfdb0cf4
lokesung <lokesung@gmail.com>
2019-01-04 23:55:03 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -4,7 +4,10 @@
 <body>
   <h1>My PHP Website</h1>
   <p>Here is some static content.</p>
-  <p><?php echo <img src='/var/html/www/HCL.png'>;
+  <p><?php 
+    <div>
+ <img src="HCL.png" alt="myPic" />
+ </div>
   ?>
 </p>
 </body>
