86a58a70b756290e3cfed3c915dc82cb3c005e8d
lokesung <lokesung@gmail.com>
2019-01-05 00:22:21 +0800

    hello

--- docker/src/index.php
+++ docker/src/index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <body>
   <h1>HCL Automation Devops</h1>
-  <p>Here is some static content.</p>
+  <p>This test site is built from Docker.</p>
   <p><img 
 src="https://cdn-images-1.medium.com/max/1200/1*CSZxfOMlVsKsrMkqTxFiMQ.png" 
 alt="Devops"></p>
