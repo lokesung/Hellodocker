0be6c758976911eb1da0c1b59d6c81b1cca78385
lokesung <lokesung@gmail.com>
2019-01-04 23:29:33 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -5,7 +5,7 @@
   <h1>My PHP Website</h1>
   <p>Here is some static content.</p>
   <p><?php echo "Here is some dynamic content";
-           echo <img src='HCL.png'>;
+           echo <img src="/var/html/www/HCL.png">;
   ?>
 </p>
 </body>
