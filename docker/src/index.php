6f219b4ba1fcc29535b8ab2770efd5a24bcf495e
lokesung <lokesung@gmail.com>
2019-01-06 01:42:28 +0800

    1

--- docker/src/index.php
+++ docker/src/index.php
@@ -2,9 +2,16 @@
 
 <html lang="en">
 <body>
-  <p><img
+  <p>
+<left>
+<img
 src="http://www.clker.com/cliparts/T/5/d/2/w/R/hcl-logo-v3.svg.med.png"
-alt="logo"></p>
+alt="logo">
+</left>
+<center>
+<h1>The time now is 0141 AM</h1>
+</center>
+</p>
   <p>This test site is auto build from Docker.</p>
   <p><img 
 src="https://cdn-images-1.medium.com/max/1200/1*CSZxfOMlVsKsrMkqTxFiMQ.png" 
